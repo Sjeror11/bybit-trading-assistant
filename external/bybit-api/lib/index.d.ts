18e61a64b22290321715d5454b949a1fb0100f5a
sjeror11 <sjeror11@gmail.com>
2025-05-25 16:51:37 +0200

    Záloha projektu, přidána adresářová struktura a .gitignore

---
+++ external/bybit-api/lib/index.d.ts
@@ -0,0 +1,7 @@
+export * from './rest-client-v5';
+export * from './spot-client-v3';
+export * from './websocket-client';
+export * from './util/logger';
+export * from './util';
+export * from './types';
+export * from './constants/enum';
---
+++ external/bybit-api/lib/util/logger.d.ts
@@ -0,0 +1,7 @@
+export type LogParams = null | any;
+export declare const DefaultLogger: {
+    /** Ping/pong events and other raw messages that might be noisy. Enable this while troubleshooting. */
+    trace: (..._params: LogParams) => void;
+    info: (...params: LogParams) => void;
+    error: (...params: LogParams) => void;
+};
